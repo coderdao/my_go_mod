d4df6fc6650f80864ff8cbcc86c3f14694a1eca0
abo <abo2013@foxmail.com>
2021-06-22 22:12:40 +0800

    up

---
+++ first_kiss.go
@@ -0,0 +1,13 @@
+package my_go_mod
+
+import "fmt"
+
+// 见面
+func Meet() {
+	fmt.Println("nice to meet you!")
+}
+
+// 再见
+func Bye() {
+	fmt.Println("I hope to see you soon!")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/coderdao/my_go_mod
+
+go 1.16
